a29b65c15db779c7bb9611d5d3b7dbae8b274c4a
Akhil Karra <akhil.karra@me.com>
2025-12-26 12:58:44 -0500

    Add main function to Test.lean to fix CI linker error

--- Test.lean
+++ Test.lean
@@ -16,3 +16,9 @@ This is your scratch space - feel free to modify or delete anything here!
 #eval LeanSandbox.addTwo 5
 
 -- Test your own definitions here
+
+-- Main function for executable
+def main : IO Unit := do
+  IO.println "Test executable - scratch space for experiments"
+  IO.println s!"hello = {LeanSandbox.hello}"
+  IO.println s!"addTwo 5 = {LeanSandbox.addTwo 5}"
